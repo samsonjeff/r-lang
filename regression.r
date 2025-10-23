89e2e71a5929b978f8d401d53e3363088d5eb6d8
blurdev <159640757+samsonjeff@users.noreply.github.com>
2025-10-23 21:14:05 +0800

    Update comments and formatting in regression.r

--- regression.r
+++ regression.r
@@ -1,5 +1,4 @@
-# SAMSON, JEFFERSON `**R programming**`
-# given dataset
+# given dataset `**R programming**`
 sales_data <- data.frame(
   Advertising = c(1889, 3844, 2326, 4553, 4772, 1269, 3111, 4861, 3339, 4832),
   Price = c(61, 64, 42, 69, 68, 41, 56, 56, 54, 54),
@@ -22,6 +21,7 @@ print(result)
 
 # Change your variable name to something unique.
 # If you are going to use my code, use this as a guide.
+
 
 
 
